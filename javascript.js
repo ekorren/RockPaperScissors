9558eafb93da186eb2bda225aac91476272c0917
ekorren <pcgame495@googlemail.com>
2022-02-10 17:02:31 +0100

    Made playerInput function deciding outcome of game

--- javascript.js
+++ javascript.js
@@ -5,15 +5,41 @@ function computerPlay(){
 let randomNum = Math.ceil(Math.random()*3)
 
 if (randomNum === 1){
-    outcomeItem = "Rock"
+    outcomeItem = "rock"
 } 
 else if (randomNum === 2){
-    outcomeItem = "Paper"
+    outcomeItem = "paper"
 }
 else {
-    outcomeItem = "Scissor"      
-} 
-console.log(outcomeItem) 
+    outcomeItem = "scissors"
+}
+}
+
+function playerInput(){
+let initialInput = prompt("Enter rock, paper or scissors")
+let input = initialInput.toLowerCase();
+if(outcomeItem === input){
+    console.log("Tie!")
+}
+else if (outcomeItem === "rock" && input === "paper"){
+console.log("You win! Paper beats rock.")
+}
+else if (outcomeItem === "paper" && input === "rock"){
+    console.log("You lose! Paper beats rock.")
+    }
+else if (outcomeItem === "rock" && input === "scissors"){
+    console.log("You lose! Rock beats Scissors.")
+    }
+else if (outcomeItem === "scissors" && input === "rock"){
+    console.log("You win! Rock beats Scissors.")
+    }
+else if (outcomeItem === "paper" && input === "scissors"){
+    console.log("You win! Scissors beats paper.")
+    }
+else if (outcomeItem === "scissors" && input === "paper"){
+    console.log("You lose! Scissors beats paper.")
+    }
 }
 
 computerPlay()
+playerInput()
